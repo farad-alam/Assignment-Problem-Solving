2d1b743390a32b2133d5126dd56ea086b84c4750
Farad Alam <farad.dev@gmail.com>
2025-02-17 23:05:39 +0600

    number 4 problem solved

--- 3 willSuccess.js
+++ 3 willSuccess.js
@@ -8,11 +8,11 @@ function willSuccess(marks) {
   return pass > fail;
 }
 
-console.log(willSuccess([60, 70, 80, 40, 30]));
-console.log(willSuccess([48, 48, 92, 100]));
-console.log(willSuccess([48, 48, 50, 50, 100]));
-console.log(willSuccess([]));
-console.log(willSuccess([90]));
-console.log(willSuccess([90, 99, 87, 48, 34, 49]));
-console.log(willSuccess("100 , 100"));
-console.log(willSuccess(90));
+// console.log(willSuccess([60, 70, 80, 40, 30]));
+// console.log(willSuccess([48, 48, 92, 100]));
+// console.log(willSuccess([48, 48, 50, 50, 100]));
+// console.log(willSuccess([]));
+// console.log(willSuccess([90]));
+// console.log(willSuccess([90, 99, 87, 48, 34, 49]));
+// console.log(willSuccess("100 , 100"));
+// console.log(willSuccess(90));
