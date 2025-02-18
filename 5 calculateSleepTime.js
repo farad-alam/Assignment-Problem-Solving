e28892f6270d1e08cc820992757b714a36b7de3d
Farad Alam <farad.dev@gmail.com>
2025-02-18 17:59:02 +0600

    5th problem solved

--- 5 calculateSleepTime.js
+++ 5 calculateSleepTime.js
@@ -1,18 +1,26 @@
 function calculateSleepTime(times) {
   let totalSecondsFromArray = 0;
-  let totalMinites = 0;
-  let totalHour = 0;
+  let finalTimes = {
+    hour: 0,
+    minute: 0,
+    second: 0,
+  };
   for (let seconds of times) {
     if (typeof seconds !== "number") {
       return "Invalid";
     }
     totalSecondsFromArray += seconds;
   }
-
-  totalMinites = totalSecondsFromArray / 60
-  totalHour = totalMinites / 60
-  console.log(totalMinites, totalHour);
-
+  finalTimes.hour = Math.floor(totalSecondsFromArray / 3600);
+  finalTimes.minute = Math.floor((totalSecondsFromArray % 3600) / 60);
+  finalTimes.second = totalSecondsFromArray % 60;
+  // console.log(Math.floor(totalMinites), Math.floor(totalHour));
+  return finalTimes;
 }
 
 console.log(calculateSleepTime([1000, 499, 519, 300]));
+console.log(calculateSleepTime([1000, 2000, 725]));
+console.log(calculateSleepTime([100, 3800]));
+console.log(calculateSleepTime([]));
+console.log(calculateSleepTime([5600]));
+console.log(calculateSleepTime([100, 3800, "90"]));
--- test.js
+++ test.js
@@ -1,3 +1,12 @@
-console.log(isNaN("101"));
-console.log(Number.isNaN("101"));
-console.log(typeof {name :"Farad"});
+var findMedianSortedArrays = function (nums1, nums2) {
+  let newArray = [...nums1, ...nums2].sort((a, b) => a - b);
+  let arrayLength = newArray.length;
+  console.log(newArray);
+  if (arrayLength % 2 === 0) {
+    return (newArray[arrayLength / 2 - 1] + newArray[arrayLength / 2]) / 2;
+  } else {
+    return newArray[Math.floor(arrayLength / 2)];
+  }
+};
+
+console.log(findMedianSortedArrays([1, 3], [2]));
